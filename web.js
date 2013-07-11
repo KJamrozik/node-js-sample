51879371558f809385a4b77d7d2e5505064f6a88
KJamrozik <kormai@wp.pl>
2013-07-11 15:33:57 +0000

    fix

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs')
+
 app.get('/', function(request, response) {
   indexContents = fs.readFileSync("./index.html", 'utf8')
   response.send(indexContents);
